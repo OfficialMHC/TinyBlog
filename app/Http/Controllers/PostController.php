aceadf26f18902d5868fba1dcc71c4cb6a636c08
OfficialMHC <official.dev.mhc@gmail.com>
2021-03-06 21:34:15 +0600

    Component nesting
    create admin & home folder into the components dir and also create sidebar, top-nav & home folder into admin & home folder. Then nesting admin-sidebar-posts-links & admin-sidebar-users-link include sidebar dir, admin-top-navbar-user-information file include top-nav, home file & blog-post file include into the home dir.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -53,7 +53,7 @@ class PostController extends Controller
 
     public function show(Post $post)
     {
-        return view('blog-post', ['post' => $post]);
+        return view('components.home.blog-post', ['post' => $post]);
     }
 
     public function edit(Post $post)
